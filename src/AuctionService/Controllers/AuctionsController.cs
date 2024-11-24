53d6dedde9c0448e49fb533fd015f7ce39103a91
Jude Adesulu <jude.adesulu@flutterwavego.com>
2024-11-24 18:16:59 +0100

    implemented RabbitMQ for service to service communication

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +16,14 @@ namespace AuctionService.Controllers
     {
         private readonly AuctionDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        public AuctionsController(AuctionDbContext context, IMapper mapper, 
+            IPublishEndpoint publishEndpoint)
         {
             _context = context;
             _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
@@ -51,9 +56,14 @@ namespace AuctionService.Controllers
             auction.Seller = "Dave";
 
             _context.Auctions.Add(auction);
+            
+            var newAuction = _mapper.Map<AuctionDto>(auction);
+            await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+            
             var result = await _context.SaveChangesAsync() > 0;
 
             if(!result) return BadRequest("Could not save chnages to DB");
+
             
             return CreatedAtAction(nameof(GetAuctionById), new {auction.Id}, _mapper.Map<AuctionDto>(auction));
         }
@@ -72,6 +82,8 @@ namespace AuctionService.Controllers
              auction.Item.Model = updateAuction.Model ?? auction.Item.Model;
              auction.Item.Mileage = updateAuction.Mileage ?? auction.Item.Mileage;
              auction.Item.Year = updateAuction.Year ?? auction.Item.Year;
+            
+             await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
 
              var result = await _context.SaveChangesAsync() > 0;
 
@@ -91,6 +103,7 @@ namespace AuctionService.Controllers
 
             _context.Auctions.Remove(auction);
             
+            await _publishEndpoint.Publish<AuctionDeleted>(new { Id = auction.Id.ToString()});
              var result = await _context.SaveChangesAsync() > 0;
 
             if(!result) return BadRequest("Could not save chnages to DB");

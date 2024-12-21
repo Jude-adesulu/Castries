3d1543d3a7a975784d6b5b559b3ca09334d52998
Jude Adesulu <jude.adesulu@flutterwavego.com>
2024-12-21 08:28:22 +0100

    Push notification implementation using signalR

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -19,11 +19,11 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
-        // cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
-        // {
-        //     h.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest")!);
-        //     h.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest")!);
-        // });
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest")!);
+            h.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest")!);
+        });
 
         cfg.ConfigureEndpoints(context);
     });
